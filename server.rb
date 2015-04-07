23a553e7462ac169c88b4a4bee4f83c619538f91
ikhakoo <ikhakoo@gmail.com>
2015-04-07 14:41:31 -0400

    working webserver

--- server.rb
+++ server.rb
@@ -1,9 +1,6 @@
 require 'socket'                                    # Require socket from Ruby Standard Library (stdlib)
 
 host = 'localhost'
-require 'socket'                                    # Require socket from Ruby Standard Library (stdlib)
-
-host = 'localhost'
 port = 2000
 
 server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
@@ -13,13 +10,32 @@ loop do                                             # Server runs forever
   client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
 
   lines = []
-  while (line = client.gets.chomp) && !line.empty?  # Read the request and collect it until it's empty
-    lines << line
+  while (line = client.gets) && !line.chomp.empty?  # Read the request and collect it until it's empty
+    lines << line.chomp
   end
   puts lines                                        # Output the full request to stdout
 
-  filename = "index.html"
-	response = File.read(filename)
+  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+
+	if File.exists?(filename)
+  	response_body = File.read(filename)
+  	success_header = []
+		success_header << "HTTP/1.1 200 OK"
+		success_header << "Content-Type: text/html" 	# should reflect the appropriate content type (HTML, CSS, text, etc)
+		success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+		success_header << "Connection: close"
+		header = success_header.join("\r\n")
+	else
+  	response_body = "File Not Found\n" 							# need to indicate end of the string with \n
+  	not_found_header = []
+		not_found_header << "HTTP/1.1 404 Not Found"
+		not_found_header << "Content-Type: text/plain" # is always text/plain
+		not_found_header << "Content-Length: #{response_body.length}" # should the actual size of the response body
+		not_found_header << "Connection: close"
+		header = not_found_header.join("\r\n")
+	end
+	
+	response = [header, response_body].join("\r\n\r\n")
 
 	client.puts(response)
   client.close                                      # Disconnect from the client
